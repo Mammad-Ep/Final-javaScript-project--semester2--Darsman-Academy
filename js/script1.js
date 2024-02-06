4fb44611a910dcfbb6c6b8005f820484d7482ce0
mammad_ep <mamad_en@yahoo.com>
2024-02-06 14:34:40 +0330

    fix problem slider in perv and next page

--- js/script1.js
+++ js/script1.js
@@ -87,12 +87,12 @@ function chat_support_site() {
 
 function scroll_anchor_link() {
     $("a[href^='#']").on('click', function (event) {
-        if (this.hash !== "") {
+        if (this.hash !== "" && this.hash!=="#carouselExampleIndicators") {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 1000, function () {
+            }, 2000, function () {
                 window.location.hash = hash;
             });
         }
